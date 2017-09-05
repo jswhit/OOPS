5ff69588312a411c221023bd5d3d8cec0bd2aedd
jswhit <whitaker.jeffrey@gmail.com>
2017-09-05 20:58:04 +0000

    update

--- oops/fv3gfs/test/fv3gfs_geom/module_fv3gfs.f90
+++ oops/fv3gfs/test/fv3gfs_geom/module_fv3gfs.f90
@@ -42,6 +42,7 @@ subroutine setup_geom(domain, gtype, nx, ny, ntiles, layout_in, io_layout_in, ha
     integer, allocatable :: tile_id(:)
     integer                              :: isc, iec, jsc, jec
     integer                              :: isd, ied, jsd, jed
+    logical is_symmetry
 
     pe = mpp_pe()
     npes = mpp_npes()
@@ -128,20 +129,57 @@ subroutine setup_geom(domain, gtype, nx, ny, ntiles, layout_in, io_layout_in, ha
         tile1(12) = 5; tile2(12) = 6
         istart1(12) = nx; iend1(12) = nx; jstart1(12) = 1;  jend1(12) = ny
         istart2(12) = 1;  iend2(12) = 1;  jstart2(12) = 1;  jend2(12) = ny
-        do n = 1, ntiles
-           tile_id(n) = n
-        enddo
-    else
-        num_contact = 1
+        is_symmetry = .true.
+    else if (gtype == 'latlon_grid') then
+        num_contact = 8
         allocate(tile1(num_contact), tile2(num_contact), tile_id(1))
         allocate(istart1(num_contact), iend1(num_contact), jstart1(num_contact), jend1(num_contact) )
         allocate(istart2(num_contact), iend2(num_contact), jstart2(num_contact), jend2(num_contact) )
+        !--- Contact line 1, between tile 1 (EAST) and tile 2 (WEST)
+        tile1(1) = 1; tile2(1) = 2
+        istart1(1) = nx; iend1(1) = nx; jstart1(1) = 1;  jend1(1) = ny
+        istart2(1) = 1;  iend2(1) = 1;  jstart2(1) = 1;  jend2(1) = ny
+        !--- Contact line 2, between tile 1 (SOUTH) and tile 3 (NORTH)  --- cyclic
+        tile1(2) = 1; tile2(2) = 3
+        istart1(2) = 1;  iend1(2) = nx; jstart1(2) = 1;   jend1(2) = 1
+        istart2(2) = 1;  iend2(2) = nx; jstart2(2) = ny;  jend2(2) = ny
+        !--- Contact line 3, between tile 1 (WEST) and tile 2 (EAST) --- cyclic
+        tile1(3) = 1; tile2(3) = 2
+        istart1(3) = 1;  iend1(3) = 1;  jstart1(3) = 1;  jend1(3) = ny
+        istart2(3) = nx; iend2(3) = nx; jstart2(3) = 1;  jend2(3) = ny
+        !--- Contact line 4, between tile 1 (NORTH) and tile 3 (SOUTH)
+        tile1(4) = 1; tile2(4) = 3
+        istart1(4) = 1;  iend1(4) = nx; jstart1(4) = ny;  jend1(4) = ny
+        istart2(4) = 1;  iend2(4) = nx; jstart2(4) = 1;   jend2(4) = 1
+        !--- Contact line 5, between tile 2 (SOUTH) and tile 4 (NORTH) --- cyclic
+        tile1(5) = 2; tile2(5) = 4
+        istart1(5) = 1;  iend1(5) = nx; jstart1(5) = 1;  jend1(5) = 1
+        istart2(5) = 1;  iend2(5) = nx; jstart2(5) = ny; jend2(5) = ny
+        !--- Contact line 6, between tile 2 (NORTH) and tile 4 (SOUTH)
+        tile1(6) = 2; tile2(6) = 4
+        istart1(6) = 1;  iend1(6) = nx; jstart1(6) = ny;  jend1(6) = ny
+        istart2(6) = 1;  iend2(6) = nx; jstart2(6) = 1;   jend2(6) = 1
+        !--- Contact line 7, between tile 3 (EAST) and tile 4 (WEST)
+        tile1(7) = 3; tile2(7) = 4
+        istart1(7) = nx; iend1(7) = nx; jstart1(7) = 1;  jend1(7) = ny
+        istart2(7) = 1;  iend2(7) = 1;  jstart2(7) = 1;  jend2(7) = ny
+        !--- Contact line 8, between tile 3 (WEST) and tile 4 (EAST) --- cyclic
+        tile1(8) = 3; tile2(8) = 4
+        istart1(8) = 1;  iend1(8) = 1;  jstart1(8) = 1;  jend1(8) = ny
+        istart2(8) = nx; iend2(8) = nx; jstart2(8) = 1;  jend2(8) = ny
+        is_symmetry = .false.
+    else
+       call mpp_error(NOTE, "setup_geom: unknown gtype "//trim(gtype))
+       return
     endif
+    do n = 1, ntiles
+       tile_id(n) = n
+    enddo
 
     call mpp_define_mosaic(global_indices, layout2D, domain, ntiles, num_contact, tile1, tile2, &
                            istart1, iend1, jstart1, jend1, istart2, iend2, jstart2, jend2,      &
                            pe_start, pe_end, whalo=halo, ehalo=halo, shalo=halo, nhalo=halo,    &
-                           symmetry=.true.,tile_id=tile_id,name=gtype)
+                           symmetry=is_symmetry,tile_id=tile_id,name=gtype)
 
     if (io_layout_in(1) /= 1 .or. io_layout_in(2) /= 1) &
         call mpp_define_io_domain(domain, io_layout_in)
